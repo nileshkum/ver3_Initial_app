4f26a639d71882340c8fea5ffa7636fa9ec90ace
Nilesh <nileshstud@gmail.com>
2023-02-10 17:06:09 -0500

    Implementing Express

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);   
+app.use(bodyParser.urlencoded());
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+
+app.use((req, res, next) => {
+    res.send('<h1> Page not found</h1>')
+});
+
+
+app.listen(3000);
