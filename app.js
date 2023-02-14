f078b2bead28e74cc7526a0d986c15f627843267
Nilesh <nileshstud@gmail.com>
2023-02-14 13:25:37 -0500

    Express JS and Middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,15 +8,18 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
 
 app.use((req, res, next) => {
-    res.send('<h1> Page not found</h1>')
+    // res.status(404).send('<h1> Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')
+    );
 });
 
 
