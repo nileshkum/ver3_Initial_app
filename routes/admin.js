ea5a8abeaf0712a7b7799b028c5234b019a156a0
Nilesh <nileshstud@gmail.com>
2023-02-14 16:48:34 -0500

    PUG Package

--- routes/admin.js
+++ routes/admin.js
@@ -5,15 +5,23 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     // res.send(
     //     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
     // );
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 router.post('/add-product', (req, res, next) => {
-    res.send('<h1> Add Product Page</h1>')
+    // res.send('<h1> Add Product Page</h1>')
+    // console.log(req.body);
+    // res.redirect('/');
+    products.push({ title: req.body.title });
+    res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
