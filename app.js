ed3e438139e240c8de033f446245f45451cc0f90
Nilesh <nileshstud@gmail.com>
2023-02-22 16:53:39 -0500

    Adding Controllers

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHBs = require('express-handlebars');
+
 
 const app = express();
 
-// app.engine('hbs', expressHBs({ layoutsDir: 'views/handlebars/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
-// app.set('view engine', 'hbs');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views/ejs');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const { ExpressHandlebars } = require('express-handlebars');
+
+const errorControllers = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/admin', adminRoutes);
-app.use('/admin', adminData.routes);
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
 
 
 
-app.use((req, res, next) => {
-    // res.status(404).send('<h1> Page not found</h1>');
-    // //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page not Found!' });
-});
+app.use(errorControllers.get404);
 
 
 app.listen(3000);
