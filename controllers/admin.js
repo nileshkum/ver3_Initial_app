4e65c2277f7bda40c2d1d6d98f76e03bde2b8b12
Nilesh <nileshstud@gmail.com>
2023-03-01 16:59:19 -0500

    Create and link edit product page

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,12 @@ const Product = require('../Model/product');
 
 exports.getAddProduct = (req, res, next) => {
 
-    res.render('admin/add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formCSS: true, productCSS: true, activeAddProduct: true });
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+
+    });
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -10,15 +15,39 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-
-
-
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 }
 
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findbyId(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+
+        });
+    });
+
+}
+
+exports.postEditProduct = (req, res, next) => {
+
+
+}
+
 exports.getProducts = (req, res, next) => {
+
     Product.fetchAll(products => {
         res.render('admin/products', {
             prods: products,
--- routes/admin.js
+++ routes/admin.js
@@ -16,6 +16,10 @@ router.get('/products', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product');
+
 
 
 module.exports = router;
